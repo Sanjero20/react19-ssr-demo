3dbab6aa5970cb1d95dd8b23910e9e873b384925
Sanjero20 <gelosantos72@gmail.com>
2025-05-20 14:04:39 +0800

    chore: initialize react router

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
 import App from "./App";
 
 hydrateRoot(
 	document.getElementById("root") as HTMLElement,
 	<StrictMode>
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</StrictMode>
 );

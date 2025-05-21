787000129c299d1a2d3c8e1b9bc669a8d4700e54
Sanjero20 <gelosantos72@gmail.com>
2025-05-21 13:11:40 +0800

    feat: custom api endpoint demo

--- src/pages/subscriptions.tsx
+++ src/pages/subscriptions.tsx
@@ -1,3 +1,31 @@
+import { useEffect, useState } from "react";
+
 export const SubscriptionsPage = () => {
-	return <div>subscriptions page</div>;
+	const [subscriptions, setSubscriptions] = useState<{ name: string; id: string }[]>([]);
+
+	const fetchData = async () => {
+		const response = await fetch("/api/subscriptions");
+		console.log(response);
+		// const users = await response.json();
+		// setSubscriptions(users);
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	return (
+		<div>
+			hello world
+			<div>
+				{subscriptions.length > 0 &&
+					subscriptions.map((subscription) => (
+						<p key={subscription.id}>
+							{subscription.name}
+							{/*  */}
+						</p>
+					))}
+			</div>
+		</div>
+	);
 };
